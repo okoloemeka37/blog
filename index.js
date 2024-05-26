e5bc892654a666225f465e20da7fb8a78e159cb1
okoloemeka37 <97386408+okoloemeka37@users.noreply.github.com>
2024-05-26 01:04:25 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+let tw=document.querySelectorAll(".tw");
+tw.forEach((rf)=>{
+    rf.addEventListener("mouseover",()=>{
+      
+            rf.style.backgroundColor="unset"
+       
+        })
+        
+        rf.addEventListener("mouseleave",()=>{
+            rf.style.backgroundColor="#00000060"
+            })
+})
+
+
+let t1=document.querySelectorAll(".t1");
+
+t1[0].style.display="block";
+
+let count=0;
+
+setInterval(() => {
+    t1[count].style.display="none"
+   
+    if (count === 2) {
+        count = -1;
+    }
+
+    count++;
+    t1[count].style.display="block"
+}, 1500);
+
+let df=document.querySelectorAll(".df");
+df[0].className="news";
+
+let drt=0;
+setInterval(() => {
+    df[drt].className="df"
+   
+    if (drt === 2) {
+        drt = -1;
+    }
+
+    drt++;
+    df[drt].className="news"
+}, 3500);
